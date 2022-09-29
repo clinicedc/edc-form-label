c73f6b5546a99d5a438c7641da85fd004d99cee0
erikvw <ew2789@gmail.com>
2022-09-29 16:08:26 -0500

    update test models

--- runtests.py
+++ runtests.py
@@ -34,6 +34,7 @@ DEFAULT_SETTINGS = DefaultTestSettings(
         "django_crypto_fields.apps.AppConfig",
         "django_revision.apps.AppConfig",
         "edc_auth.apps.AppConfig",
+        "edc_action_item.apps.AppConfig",
         "edc_crf.apps.AppConfig",
         "edc_appointment.apps.AppConfig",
         "edc_metadata.apps.AppConfig",
