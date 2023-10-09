4a7552cf24be7c5de42c25473181f2e40bb560b1
erikvw <ew2789@gmail.com>
2023-10-09 16:47:15 -0500

    update paths from edc-visit-schedule, setup

--- edc_form_label/tests/tests/test_form_label.py
+++ edc_form_label/tests/tests/test_form_label.py
@@ -12,7 +12,7 @@ from edc_facility import import_holidays
 from edc_reference import site_reference_configs
 from edc_registration.models import RegisteredSubject
 from edc_utils import get_utcnow
-from edc_visit_schedule import site_visit_schedules
+from edc_visit_schedule.site_visit_schedules import site_visit_schedules
 from edc_visit_tracking.constants import SCHEDULED
 
 from ...custom_label_condition import CustomLabelCondition
